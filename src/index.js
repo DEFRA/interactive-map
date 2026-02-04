9e69fea3d550e233c856531ecb481a4d373fdcec
Alex Luckett <alexgluckett@gmail.com>
2026-02-04 15:09:53 +0000

    remove unused export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// Re-export types for consumers
-// This allows: import('@defra/interactive-map').PluginDescriptor
-export * from './types.js'
-
 // Export the main class
 export { default } from './InteractiveMap/InteractiveMap.js'
 export { default as InteractiveMap } from './InteractiveMap/InteractiveMap.js'
