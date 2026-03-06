5a9366a28ff1f6f97aa4661ce3fb4ca63d910a10
Alex Luckett <alexgluckett@gmail.com>
2026-03-06 12:15:21 +0000

    chore: add Storybook preview config with viewport presets

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    viewport: {
+      viewports: {
+        ...INITIAL_VIEWPORTS,
+        mapMobile: {
+          name: 'Map Mobile (375px)',
+          styles: { width: '375px', height: '812px' }
+        },
+        mapDesktop: {
+          name: 'Map Desktop (1280px)',
+          styles: { width: '1280px', height: '800px' }
+        }
+      }
+    }
+  }
+}
+
+export default preview
