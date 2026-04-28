979268410395e0fe0959304513d6ede75ffa57dd
Mark Fee <git@markfee.com>
2026-04-28 13:46:48 +0100

    IM-224 setStyle and setSubLayerStyle use mappedDatasets (#243)
    * IM-224 added mappedDatasetsReducer tests
    
    * IM-224 changed signature parameter name on getPatternImageId
    
    * IM-224 Temp console.log effect in Layers.jsx
    
    * IM-224 added some timeout changes to styles/visibility to test state changes
    
    * IM-224 WIP - attempt to refactor setStyle to use mappedDatasets
    
    * Revert IM-224 changed signature parameter name on getPatternImageId
    
    * IM-224 added a demo timeout change of style
    
    * IM-224 setStyle and setSubLayerStyle use mappedDatasets
    
    * IM-224 fixed tests after change to setSublayerStyle
    
    * IM-224 simplified setStyle
    
    * IM-224 altered test to cover new sublayer format too
    
    * IM-224 removed BUILD_MAPPED_DATASETS listener

--- demo/js/index.js
+++ demo/js/index.js
@@ -345,7 +345,17 @@ interactiveMap.on('map:ready', function (e) {
 interactiveMap.on('datasets:ready', function () {
 	// setTimeout(() => datasetsPlugin.setFeatureVisibility(false, [55], { datasetId: 'land-covers', idProperty: null }), 2000)
 	// setTimeout(() => datasetsPlugin.setFeatureVisibility(true, [55], { datasetId: 'land-covers', idProperty: null }), 4000)
-	// setTimeout(() => datasetsPlugin.setStyle({ stroke: { outdoor: '#ff0000', dark: '#ffffff' }, fillPattern: 'horizontal-hatch', fillPatternForegroundColor: { outdoor: '#ff0000', dark: '#ffffff' } }, { datasetId: 'land-covers', sublayerId: '130' }), 2000)
+
+	setTimeout(() => datasetsPlugin.setStyle(
+		{ 
+			stroke: { outdoor: '#ff0000', dark: '#ffffff' }, 
+			fillPattern: 'horizontal-hatch', 
+			fillPatternForegroundColor:  { outdoor: '#ff0000', dark: '#ffffff' },
+			fillPatternBackgroundColor: 'transparent'
+		}, 
+		{ datasetId: 'land-covers', sublayerId: '130-131' }), 2000)
+	// setTimeout(() => datasetsPlugin.setDatasetVisibility(true, { datasetId: 'hedge-control' }), 500)
+	// setTimeout(() => datasetsPlugin.setStyle({ stroke: { outdoor: '#ffff00' }, }, { datasetId: 'hedge-control' }), 1000)
 })
 
 // Ref to the selected features
