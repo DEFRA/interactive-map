e49479ae30969cd97785da515267c7e4e1ededbb
Dan Leech <dan.leech@defra.gov.uk>
2026-05-19 11:55:40 +0100

    Lint fix

--- providers/beta/openlayers/src/utils/tileLayers.test.js
+++ providers/beta/openlayers/src/utils/tileLayers.test.js
@@ -2,7 +2,7 @@ import XYZ from 'ol/source/XYZ.js'
 import TileGrid from 'ol/tilegrid/TileGrid.js'
 import VectorTileSource from 'ol/source/VectorTile.js'
 import VectorTileLayer from 'ol/layer/VectorTile.js'
-import { stylefunction, recordStyleLayer } from 'ol-mapbox-style'
+import { stylefunction } from 'ol-mapbox-style'
 import { createTileSource, createVectorTileLayer } from './tileLayers.js'
 import { TILE_GRID_RESOLUTIONS, TILE_GRID_ORIGIN, TILE_SIZE } from '../defaults.js'
 
