90c4db02d1599a8dcebb8df01a24e348e05fe25a
Mark Fee <mark.fee@defra.gov.uk>
2026-05-19 17:48:33 +0100

    IM-286 dataset visibility simplified

--- plugins/beta/datasets/src/api/setStyle.js
+++ plugins/beta/datasets/src/api/setStyle.js
@@ -3,9 +3,8 @@ export const setStyle = ({ pluginState, mapState }, styleChanges, { datasetId, s
   if (!dataset) {
     return
   }
+  datasetId = sublayerId ? `${datasetId}-${sublayerId}` : datasetId
   const mapStyle = mapState.mapStyle
-  // const type = sublayerId ? 'SET_SUBLAYER_STYLE' : 'SET_DATASET_STYLE'
-  const type = 'SET_DATASET_STYLE'
-  const payload = { datasetId: sublayerId ? `${datasetId}-${sublayerId}` : datasetId, styleChanges, mapStyle, sublayerId }
-  pluginState.dispatch({ type, payload })
+  const payload = { datasetId, styleChanges, mapStyle, sublayerId }
+  pluginState.dispatch({ type: 'SET_DATASET_STYLE', payload })
 }
