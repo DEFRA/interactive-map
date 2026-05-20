a634c30bc5b0f32514d04a765b412405cd523595
Mark Fee <mark.fee@defra.gov.uk>
2026-05-20 16:21:35 +0100

    IM-291 added KeyItem.jsx and KeyGroupItem.jsx

--- plugins/beta/datasets/src/panels/Key.jsx
+++ plugins/beta/datasets/src/panels/Key.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { EmptyKey } from '../components/key/EmptyKey.jsx'
-import { KeyItem } from './key/KeyItem.jsx'
+import { KeyItem } from '../components/key/KeyItem.jsx'
+import { KeyGroupItem } from '../components/key/KeyGroupItem.jsx'
 import { datasetRegistry } from '../registry/datasetRegistry.js'
 
 export const Key = ({
@@ -17,32 +18,34 @@ export const Key = ({
 
   const containerClass = `im-c-datasets-key${hasGroups ? ' im-c-datasets-key--has-groups' : ''}`
   return (
-    <div className={containerClass}>
-      {keyGroups.map(item => {
-        if (item.type === 'sublayers') {
-          const headingId = `key-heading-${item.dataset.id}`
-          return (
-            <section key={item.dataset.id} className='im-c-datasets-key__group' aria-labelledby={headingId}>
-              <h3 id={headingId} className='im-c-datasets-key__group-heading'>{item.dataset.label}</h3>
-              {item.sublayers.map(sublayer =>
-                <KeyItem key={`${sublayer.id}`} registryDataset={sublayer} symbolRegistry={symbolRegistry} patternRegistry={patternRegistry} mapStyle={mapStyle} />
-              )}
-            </section>
-          )
-        }
+    <div className={containerClass}>{keyGroups.map(item => {
+      const key = item.type === 'group' ? item.groupLabel.toLowerCase().replaceAll(/\s+/g, '-') : item.dataset.id
 
-        if (item.type === 'group') {
-          const headingId = `key-heading-${item.groupLabel.toLowerCase().replaceAll(/\s+/g, '-')}`
-          return (
-            <section key={item.groupLabel} className='im-c-datasets-key__group' aria-labelledby={headingId}>
-              <h3 id={headingId} className='im-c-datasets-key__group-heading'>{item.groupLabel}</h3>
-              {item.datasets.map(dataset => <KeyItem key={`${dataset.id}`} registryDataset={dataset} symbolRegistry={symbolRegistry} patternRegistry={patternRegistry} mapStyle={mapStyle} />)}
-            </section>
-          )
-        }
-
-        return <KeyItem key={`${item.dataset.id}`} registryDataset={item.dataset} symbolRegistry={symbolRegistry} patternRegistry={patternRegistry} mapStyle={mapStyle} />
-      })}
+      if (item.type === 'sublayers' || item.type === 'group') {
+        const datasets = item.type === 'sublayers' ? item.sublayers : item.datasets
+        const label = item.type === 'sublayers' ? item.dataset.label : item.groupLabel
+        return (
+          <KeyGroupItem
+            key={key}
+            headingId={`key-heading-${key}`}
+            label={label}
+            datasets={datasets}
+            symbolRegistry={symbolRegistry}
+            patternRegistry={patternRegistry}
+            mapStyle={mapStyle}
+          />
+        )
+      }
+      return (
+        <KeyItem
+          key={key}
+          registryDataset={item.dataset}
+          symbolRegistry={symbolRegistry}
+          patternRegistry={patternRegistry}
+          mapStyle={mapStyle}
+        />
+      )
+    })}
     </div>
   )
 }
