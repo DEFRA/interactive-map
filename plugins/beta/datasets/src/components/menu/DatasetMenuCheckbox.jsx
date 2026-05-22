4bff553dbc3e0d991740a35923f675258731277a
Mark Fee <mark.fee@defra.gov.uk>
2026-05-22 13:52:58 +0100

    IM-294 Replaced Layers.jsx with DatasetMenu.jsx

---
+++ plugins/beta/datasets/src/components/menu/DatasetMenuCheckbox.jsx
@@ -0,0 +1,32 @@
+import { datasetRegistry } from '../../registry/datasetRegistry.js'
+
+export const DatasetMenuCheckbox = ({ menuGroupItem, onChange }) => {
+  const registryDataset = datasetRegistry.getDataset(menuGroupItem.id)
+  if (!registryDataset) {
+    return null
+  }
+  const datasetId = registryDataset.isSublayer ? registryDataset.parentId : registryDataset.id
+  const sublayerId = registryDataset.isSublayer ? registryDataset.id : undefined
+
+  const itemClass = `im-c-datasets-layers__item govuk-checkboxes govuk-checkboxes--small${registryDataset.visible ? '' : ' im-c-datasets-layers__item--checked'}`
+  return (
+    <div key={registryDataset.id} className={itemClass} data-module='govuk-checkboxes'>
+      <div className='govuk-checkboxes__item'>
+        <input
+          className='govuk-checkboxes__input'
+          id={registryDataset.id}
+          data-dataset-id={datasetId}
+          data-sublayer-id={sublayerId}
+          name='layers'
+          type='checkbox'
+          value={registryDataset.id}
+          checked={registryDataset.visible}
+          onChange={onChange}
+        />
+        <label className='im-c-datasets-layers__item-label govuk-label govuk-checkboxes__label' htmlFor={registryDataset.id}>
+          {registryDataset.label}
+        </label>
+      </div>
+    </div>
+  )
+}
---
+++ plugins/beta/datasets/src/components/menu/DatasetMenuGroupWrapper.jsx
@@ -0,0 +1,19 @@
+export const DatasetMenuGroupWrapper = ({ menuGroup, children }) => {
+  if (!menuGroup.groupLabel) {
+    return <>{children}</>
+  }
+  // TODO - is there any need for im-c-datasets-layers-group--items-checked - it isn't used anywhere
+  // const anyItemsChecked = false
+  // const wrapperClass = `govuk-form-group im-c-datasets-layers-group${anyItemsChecked ? ' im-c-datasets-layers-group--items-checked' : ''}`
+  const wrapperClass = 'govuk-form-group im-c-datasets-layers-group'
+  return (
+    <div key={menuGroup.id} className={wrapperClass}>
+      <fieldset className='im-c-datasets-layers-group__fieldset'>
+        <legend className='im-c-datasets-layers-group__legend'>
+          {menuGroup.groupLabel}
+        </legend>
+        {children}
+      </fieldset>
+    </div>
+  )
+}
