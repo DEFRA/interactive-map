6a45fc615d30761ee8ea97a2d54695b6612dbbd7
Mark Fee <mark.fee@defra.gov.uk>
2026-06-01 10:21:55 +0100

    IM-320 moved datasetDefinitionCache to its own file

--- plugins/beta/datasets/src/registry/datasetRegistry.js
+++ plugins/beta/datasets/src/registry/datasetRegistry.js
@@ -1,40 +1,5 @@
 import { createDataset } from './createDataset.js'
-
-class DatasetDefinitionCache {
-  constructor () {
-    this.idToDefinitionMap = new Map()
-    this.definitionToInstanceMap = new Map()
-  }
-
-  add (registryDataset) {
-    this.idToDefinitionMap.set(registryDataset.id, registryDataset._datasetDefinition)
-    this.definitionToInstanceMap.set(registryDataset._datasetDefinition, registryDataset)
-  }
-
-  invalidateDataset (registryDataset) {
-    const existingDefinition = this.idToDefinitionMap.get(registryDataset.id)
-    const allIds = [existingDefinition.id, ...(existingDefinition?.sublayerIds || [])]
-    allIds.forEach(id => {
-      const existingDefinition = this.idToDefinitionMap.get(id)
-      this.definitionToInstanceMap.delete(existingDefinition)
-      this.idToDefinitionMap.delete(id)
-    })
-  }
-
-  invalidateChangedDatasets (newDatasets) {
-    newDatasets.forEach((datasetDefinition) => {
-      const existingDefinition = this.idToDefinitionMap.get(datasetDefinition.id)
-      if (existingDefinition && existingDefinition !== datasetDefinition) {
-        const instance = this.definitionToInstanceMap.get(existingDefinition)
-        this.invalidateDataset(instance.isSublayer && instance.parent ? instance.parent : instance)
-      }
-    })
-  }
-
-  getByDefinition (datasetDefinition) {
-    return this.definitionToInstanceMap.get(datasetDefinition)
-  }
-}
+import { DatasetDefinitionCache } from './datasetDefinitionCache.js'
 
 const datasetRegistry = {
   attach (datasetsRef, orderedDatasetsRef) {
