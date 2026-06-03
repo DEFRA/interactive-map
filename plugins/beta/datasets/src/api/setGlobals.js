b906772cc0f8e183eaa12ec92861a38cf38e1be1
Mark Fee <mark.fee@defra.gov.uk>
2026-06-03 14:58:38 +0100

    IM-333 added an api for setGlobals

---
+++ plugins/beta/datasets/src/api/setGlobals.js
@@ -0,0 +1,20 @@
+import { logger } from '../../../../../src/services/logger.js'
+export const setGlobals = ({ pluginState: { dispatch } }, values) => {
+  const { opacityMode } = values
+  const payload = {}
+  let valuesSet = false
+  if (opacityMode) {
+    if (!['dataset', 'global', 'multiply'].includes(opacityMode)) {
+      logger.warn(`Ignoring invalid opacityMode: ${opacityMode}. Must be one of 'dataset', 'global' or 'multiply'.`)
+    } else {
+      valuesSet = true
+      payload.opacityMode = opacityMode
+    }
+  }
+
+  if (valuesSet) {
+    dispatch({ type: 'SET_GLOBAL_STATE', payload })
+  } else {
+    logger.warn('setGlobals: requires a valid value for opacityMode to be set')
+  }
+}
