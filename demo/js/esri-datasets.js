9f3a1e14acc53977f49d131cba08e19c82a2ce1a
Mark Fee <mark.fee@defra.gov.uk>
2026-06-15 12:10:55 +0100

    IM-360 Very basic flood zone layer rendering

--- demo/js/esri-datasets.js
+++ demo/js/esri-datasets.js
@@ -7,14 +7,51 @@ import createDatasetsPlugin from '/plugins/beta/datasets/src/index.js'
 import { vtsMapStyles27700 } from './mapStyles.js'
 import { transformGeocodeRequest, transformVtsRequest3857, setupEsriConfig } from './auth.js'
 
+const datasets = [
+  {
+    id: 'flood-zones',
+    label: 'Flood Zones',
+    groupLabel: 'Datasets',
+    tiles: `https://tiles.arcgis.com/tiles/JZM7qJpmv7vJ0Hzx/arcgis/rest/services/Flood_Zones_2_and_3_Rivers_and_Sea_NON_PRODUCTION/VectorTileServer`,
+    showInKey: true,
+    showInMenu: true,
+    sublayers: [
+      {
+        id: 'flood-zone-2',
+        label: 'Flood Zone 2',
+        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 2/1',
+        showInKey: true,
+        showInMenu: false,
+        style: {
+          fill: { outdoor: '#1d70b8', dark: '#7fcdbb' },
+          stroke: 'none'
+        },
+      },
+      {
+        id: 'flood-zone-3',
+        label: 'Flood Zone 3',
+        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 3/1',
+        showInKey: true,
+        showInMenu: false,
+        style: {
+          fill: { outdoor: '#003078', dark: '#e5f5e0' },
+          stroke: 'none'
+        },
+      }
+    ]
+  }
+]
+
 const datasetsPlugin = createDatasetsPlugin({
   globals: {
     opacityMode: 'global', // 'dataset', 'global' or 'multiply'
     opacity: 0.75,
     visible: true
   },
-  datasets: []
+  datasets
 })
+
+
 
 const interactiveMap = new InteractiveMap('map', {
   behaviour: 'mapOnly',
@@ -29,8 +66,8 @@ const interactiveMap = new InteractiveMap('map', {
     mapStylesPlugin({
       mapStyles: vtsMapStyles27700,
       manifest: {
-        buttons: [{ 
-          id: 'mapStyles', 
+        buttons: [{
+          id: 'mapStyles',
           desktop: { slot: 'right-top', showLabel: true }
         }],
         panels: [
