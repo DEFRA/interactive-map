391b528ebde01b8d812a8417f7ab4e462a506173
Mark Fee <mark.fee@defra.gov.uk>
2026-06-17 16:57:22 +0100

    IM-360 replaced styleLayerId with esriStyleLayerId

--- demo/js/esri-datasets.js
+++ demo/js/esri-datasets.js
@@ -22,8 +22,7 @@ const datasets = [
       {
         id: 'climate-change',
         label: 'Climate change (2070 to 2125)',
-        // TODO change styleLayerId to esriStyleLayerId
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Flood Zones plus climate change/1',
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Flood Zones plus climate change/1',
         showInKey: true,
         showInMenu: false,
         style: {
@@ -47,21 +46,21 @@ const datasets = [
         style: {
           stroke: { outdoor: '#000000', dark: '#FFFFFF' },
         },
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/0',
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/0',
         showInKey: false,
         showInMenu: false,
       },
       {
         id: 'data-unavailable-light',
         visibleWhen: { mapStyleId: ['outdoor', 'black-and-white'] },
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/1',
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/1',
         showInKey: false,
         showInMenu: false,
       },
       {
         id: 'data-unavailable-dark',
         visibleWhen: { mapStyleId: ['dark'] },
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/2',
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea CCP1/Unavailable/2',
         showInKey: false,
         showInMenu: false,
       }
@@ -74,14 +73,14 @@ const datasets = [
     esriGroupId: 'flood-zones',
     tiles: `https://tiles.arcgis.com/tiles/JZM7qJpmv7vJ0Hzx/arcgis/rest/services/Flood_Zones_2_and_3_Rivers_and_Sea_NON_PRODUCTION/VectorTileServer`,
     showInKey: true,
-    showInMenu: true,
+    // showInMenu: true,
     sourceLayer: 'Flood Zones 2 and 3 Rivers and Sea',
     sublayers: [
       {
         id: 'flood-zone-2',
         label: 'Flood Zone 2',
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 2/1',
-        showInMenu: false,
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 2/1',
+        showInMenu: true,
         style: {
           // filter: ['==', 'flood_zone', 'FZ2'],
           fill: { outdoor: '#1d70b8', dark: '#7fcdbb' },
@@ -91,8 +90,8 @@ const datasets = [
       {
         id: 'flood-zone-3',
         label: 'Flood Zone 3',
-        styleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 3/1',
-        showInMenu: false,
+        esriStyleLayerId: 'Flood Zones 2 and 3 Rivers and Sea/Flood Zone 3/1',
+        showInMenu: true,
         style: {
           // filter: ['==', 'flood_zone', 'FZ3'],
           fill: { outdoor: '#003078', dark: '#e5f5e0' },
