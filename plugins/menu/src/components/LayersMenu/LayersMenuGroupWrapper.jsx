8c8cc8b9d4955a0ef9954b0a78a908d3eb11cf55
Mark Fee <mark.fee@defra.gov.uk>
2026-08-19 14:57:41 +0100

    IM-439 copied layersMenu from datasets

---
+++ plugins/menu/src/components/LayersMenu/LayersMenuGroupWrapper.jsx
@@ -0,0 +1,18 @@
+export const LayersMenuGroupWrapper = ({ menuGroup, children }) => {
+  if (!menuGroup.groupLabel) {
+    return <>{children}</>
+  }
+  const wrapperClass = 'govuk-form-group im-c-menu-layers-group'
+  return (
+    <div key={menuGroup.id} className={wrapperClass}>
+      <fieldset className='im-c-menu-layers-group__fieldset'>
+        <legend className='im-c-menu-layers-group__legend'>
+          <h3>
+            {menuGroup.groupLabel}
+          </h3>
+        </legend>
+        {children}
+      </fieldset>
+    </div>
+  )
+}
